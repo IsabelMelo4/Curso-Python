465a691c448efc7c76e1c2ac0d9a1b5288ce99e6
isabelMelo4 <isabelmelo630@gmail.com>
2025-06-19 11:28:46 -0300

    organizei as pastas

---
+++ Função/como declarar funcao.py
@@ -0,0 +1,14 @@
+
+# definição de função 
+
+def mensagem():
+    print ("olá")
+
+#progama principal
+
+mensagem()
+
+#   "def "serve para definimos uma função, e nos podemos usar ela depois para outros progamas 
+
+# " mensagem () " é o nome da nossa função sempre que chamarmos ela no codigo exatamente assim, ela executa o bloco 
+
